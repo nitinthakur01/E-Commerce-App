8ef02382f0026ba448108801f00ecf7ec1f9262b
Nitin Thakur <nitinthakur46323@gmail.com>
2025-04-09 11:32:27 +0530

    address server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const authRouter = require("./routes/auth/auth-routes");
 const adminProductRouter = require("./routes/admin/products-routes");
 const shopProductRouter = require("./routes/shop/products-routes");
 const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -33,6 +34,7 @@ app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductRouter);
 app.use("/api/shop/products", shopProductRouter);
 app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
 
 connectDb().then(() => {
   app.listen(PORT, () => {
