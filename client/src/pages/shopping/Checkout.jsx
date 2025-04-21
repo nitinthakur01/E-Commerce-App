328856e90c29ba3a455ffc7df3fc4582e80350a3
Nitin Thakur <nitinthakur46323@gmail.com>
2025-04-21 11:27:01 +0530

    checkout page

--- client/src/pages/shopping/Checkout.jsx
+++ client/src/pages/shopping/Checkout.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import img from "../../assets/account.jpg";
+import Address from "@/components/shopping/Address";
+import { useSelector } from "react-redux";
+import UserCartItemContent from "@/components/shopping/cartItemContent";
+import { Button } from "@/components/ui/button";
 
 const ShoppingCheckout = () => {
-  return <div>ShoppingCheckout</div>;
+  const { cartItems } = useSelector((state) => state.shopCart);
+
+  const totalCartAmount =
+    cartItems && cartItems.items && cartItems.items.length > 0
+      ? cartItems.items.reduce(
+          (sum, currentItem) =>
+            sum +
+            (currentItem?.salePrice > 0
+              ? currentItem?.salePrice
+              : currentItem?.price) *
+              currentItem?.quantity,
+          0
+        )
+      : 0;
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img src={img} className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 mt-5 p-5">
+        <Address />
+        <div className="flex flex-col gap-4">
+          {cartItems && cartItems.items && cartItems.items.length > 0
+            ? cartItems.items.map((item) => (
+                <UserCartItemContent cartItem={item} />
+              ))
+            : null}
+          <div className="mt-8 space-y-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Total</span>
+              <span className="font-bold">${totalCartAmount}</span>
+            </div>
+          </div>
+          <div className="mt-4 w-full">
+            <Button className="w-full">Checkout with Paypal</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ShoppingCheckout;
