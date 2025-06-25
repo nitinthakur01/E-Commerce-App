8277575fea9d4f1b9e5f6cda6bcaac9c2c88acd4
Nitin Thakur <nitinthakur46323@gmail.com>
2025-06-25 14:40:07 +0530

    review server

---
+++ server/controllers/shop/review-controller.js
@@ -0,0 +1,84 @@
+const Order = require("../../models/Order");
+const Product = require("../../models/Product");
+const Review = require("../../models/Review");
+
+const addProductReview = async (req, res) => {
+  try {
+    const { productId, userId, userName, reviewMessage, reviewValue } =
+      req.body;
+
+    const order = await Order.findOne({
+      userId,
+      "cartItems.productId": productId,
+      orderStatus: "confirmed",
+    });
+
+    if (!order) {
+      return res.status(403).json({
+        success: false,
+        message: "You need to purchase product to review it.",
+      });
+    }
+
+    const checkExistinfReview = await Review.findOne({
+      productId,
+      userId,
+    });
+
+    if (checkExistinfReview) {
+      return res.status(400).json({
+        succes: false,
+        message: "You already reviewed this product!",
+      });
+    }
+
+    const newReview = new Review({
+      productId,
+      userId,
+      userName,
+      reviewMessage,
+      reviewValue,
+    });
+
+    await newReview.save();
+
+    const reviews = await Review.find({ productId });
+    const totalReviewsLength = reviews.length;
+    const averageReview =
+      reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+      totalReviewsLength;
+
+    await Product.findByIdAndUpdate(productId, { averageReview });
+
+    res.status(201).json({
+      success: true,
+      data: newReview,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+const getProductReviews = async (req, res) => {
+  try {
+    const { productId } = req.params;
+
+    const reviews = await Review.find({ productId });
+    res.status(200).json({
+      success: true,
+      data: reviews,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+};
+
+module.exports = { addProductReview, getProductReviews };
